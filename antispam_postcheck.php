8ab054ffe7aaa7f6ec972fbcaafb2fb47c74d1ea
Denys Fedoryshchenko <nuclearcat@nuclearcat.com>
2020-12-10 17:58:42 +0200

    Different colors if IP is in blacklists

--- antispam_postcheck.php
+++ antispam_postcheck.php
@@ -2,8 +2,8 @@
 // https://github.com/roundcube/roundcubemail/wiki/Plugin-Hooks#task-mail
 class antispam_postcheck extends rcube_plugin
 {
-	public $task = 'mail';
-	
+    public $task = 'mail';
+
     public function init()
     {
         $rcmail = rcmail::get_instance();
@@ -13,58 +13,76 @@ class antispam_postcheck extends rcube_plugin
     }
     public function storage_init($p)
     {
-        $rcmail = rcmail::get_instance();
-        $add_headers = array('Received');
+        $rcmail             = rcmail::get_instance();
+        $add_headers        = array('Received');
         $p['fetch_headers'] = trim($p['fetch_headers'] . ' ' . strtoupper(join(' ', $add_headers)));
         return $p;
     }
     public function message_objects($p)
     {
-        $rcmail = rcmail::get_instance();
-        $ignore_received  = $rcmail->config->get('antispam_ignore_received');
-		if (isset($p['message']->headers->others['received'])) {
-			$n = 0;
-			
-			while (isset($p['message']->headers->others['received'][$n])) {
-				foreach ($ignore_received as $value) {
-					if (strpos($p['message']->headers->others['received'][$n], $value) !== false) {
-						$n++;
-						continue;						
-					}
-				}
-				break;
-			}
-			if (preg_match("/^from (\S+) \((\S+) \[(\S+)\]\).*/", $p['message']->headers->others['received'][$n], $array_parsed)) {
-				$octets = explode('.', $array_parsed[3]);
-				$name_lookup = $octets[3].".".$octets[2].".".$octets[1].".".$octets[0].".origin.asn.cymru.com";
-				$ret = dns_get_record($name_lookup,  DNS_TXT);
-				if (isset($ret[0]['entries'][0])) {
-					$display = $array_parsed[3]. " ASINFO:" .$ret[0]['entries'][0];
-					if (preg_match("/^([0-9]+) /", $ret[0]['entries'][0], $arr_asn)) {
-						$name_lookup = "AS".$arr_asn[1].".asn.cymru.com";
-						$ret = dns_get_record($name_lookup,  DNS_TXT);
-						if (isset($ret[0]['entries'][0])) {
-							$display .= " ASNAME:" .$ret[0]['entries'][0];
-						}
-						
-					}
-				}
-			} else {
-				$display = $p['message']->headers->others['received'][$n];
-			}
-    		$attrib['id']    = 'antispam-infobox';
-    		$attrib['class'] = 'notice';
-    		//$attrib['style'] = 'display: none';
+        $rcmail          = rcmail::get_instance();
+        $ignore_received = $rcmail->config->get('antispam_ignore_received');
+        if (isset($p['message']->headers->others['received'])) {
+            $n = 0;
 
-			$msg = html::span(null, rcube::Q($display));
-			$p['content'][] = html::div($attrib, $msg . '&nbsp;');
+            while (isset($p['message']->headers->others['received'][$n])) {
+                foreach ($ignore_received as $value) {
+                    if (strpos($p['message']->headers->others['received'][$n], $value) !== false) {
+                        $n++;
+                        continue;
+                    }
+                }
+                break;
+            }
+            $color_box = "#cccccc";
+            if (preg_match("/^from (\S+) \((\S+) \[(\S+)\]\).*/", $p['message']->headers->others['received'][$n], $array_parsed)) {
+                $octets      = explode('.', $array_parsed[3]);
+                $name_lookup = $octets[3] . "." . $octets[2] . "." . $octets[1] . "." . $octets[0] . ".origin.asn.cymru.com";
+                $ret         = dns_get_record($name_lookup, DNS_TXT);
+                if (isset($ret[0]['entries'][0])) {
+                    $display = $array_parsed[3] . " ASINFO:" . $ret[0]['entries'][0];
+                    if (preg_match("/^([0-9]+) /", $ret[0]['entries'][0], $arr_asn)) {
+                        $name_lookup = "AS" . $arr_asn[1] . ".asn.cymru.com";
+                        $ret         = dns_get_record($name_lookup, DNS_TXT);
+                        if (isset($ret[0]['entries'][0])) {
+                            $display .= " ASNAME:" . $ret[0]['entries'][0];
+                        }
+                    }
+                }
+                $name_lookup = $octets[3] . "." . $octets[2] . "." . $octets[1] . "." . $octets[0] . ".dnsbl-1.uceprotect.net";
+                $ret         = dns_get_record($name_lookup, DNS_A);
+                
+                if (isset($ret[0])) {
+                    $color_box = '#FF0000';
+                    //$display .= " ADD ".$ret[0]['entries'][0];
+                }
 
-			/*
-			$p['content'][] = html::p(array('class' => 'aligned-buttons boxinformation'),
-                    html::span(null, rcube::Q($display))
-                );
-			*/
-		}
-    	return($p);
+                if ($color_box == "#cccccc") {
+                    $name_lookup = $octets[3] . "." . $octets[2] . "." . $octets[1] . "." . $octets[0] . ".bl.mailspike.net";
+                    $ret         = dns_get_record($name_lookup, DNS_A);
+                    if (isset($ret[0])) {
+                        $color_box = 'pink';
+                        //$display .= " ADD ".$ret[0]['entries'][0];
+                    }
+
+                }
+
+            } else {
+                $display = $p['message']->headers->others['received'][$n];
+            }
+            $attrib['id']    = 'antispam-infobox';
+            $attrib['class'] = 'notice';
+            $attrib['style'] = 'background-color: ' . $color_box . ';';
+
+            $msg            = html::span(null, rcube::Q($display));
+            $p['content'][] = html::div($attrib, $msg . '&nbsp;');
+
+            /*
+        $p['content'][] = html::p(array('class' => 'aligned-buttons boxinformation'),
+        html::span(null, rcube::Q($display))
+        );
+         */
+        }
+        return ($p);
     }
 }
